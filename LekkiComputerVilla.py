47e9562d74851e87dea864109eb21b1634801dee
unknown <Oluwabunmi>
2022-08-19 14:48:34 +0200

    just added LekkiComputerVilla.py and index.html

---
+++ LekkiComputerVilla.py
@@ -0,0 +1,29 @@
+print("")
+Students ={"Momo": 700, "Ayomide": 700, "Lucien": 900, "Alexandar": 800, "Jessica": 1000}
+
+print("................Students and their grade..............")
+print('')
+print("Momo: ", Students["Momo"])
+print("Ayomide: ", Students["Ayomide"])
+print("Lucien: ", Students["Lucien"])
+print("Alexandar: ", Students["Alexandar"])
+print("Jessica: ", Students["Jessica"])
+
+
+print("")
+print("...............The Three Best Student,They Ranks and Grades............")
+print("Jessica: ", "1st", Students["Jessica"])
+print("Lucien: ", "2nd", Students["Lucien"])
+print("Alexandar: ", "1st", Students["Alexandar"])
+
+
+print("")
+print("Jessica you are rewarded with #100,000 for securing the 1st position in your class")
+print("Lucien you are rewarded with #50,000 for securing the 2nd position in your class")
+print("Alexandar you are rewarded with #30,000 for securing the 3rd position in your class")
+
+print('')
+print("Jessica: ", "Congratuation! keep it up")
+print("Lucien: ", "Congratuation! keep it up")
+print("Alexandar: ", "Congratuation! keep it up")
+
